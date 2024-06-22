17ea4e6d89ba92ae85b54c3aa6dad3a00023384c
thbttrd <thbttrd.dev@pm.me>
2024-06-22 18:12:26 +0200

    Add app files

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('vite');
+
+export default defineConfig(async () => {
+  const { viteStaticCopy } = await import('vite-plugin-static-copy');
+  
+  return {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'images/*',
+            dest: 'assets/images/'
+          }
+        ]
+      })
+    ]
+  };
+});
